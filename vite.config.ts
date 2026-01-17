8e48a554db9122a849944f169046d3d55cb72e7d
victor117-byte <victor117.berrios@gmail.com>
2026-01-16 20:30:44 -0600

    Changes to be committed: 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['.ngrok.io'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
